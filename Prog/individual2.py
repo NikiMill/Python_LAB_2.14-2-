19d390b097a2dae9b7757049c1abba9f5c8b22e1
NikiMill <108574543+NikiMill@users.noreply.github.com>
2023-12-11 11:05:16 +0300

    Update individual2.py
    дурак я

--- Prog/individual2.py
+++ Prog/individual2.py
@@ -5,69 +5,23 @@
 #Обнаружив его, программа должна удалить все содержимое, начиная с этого символа и до конца строки. 
 #Для простоты не будем рассматривать ситуации, когда знак решетки встречается в середине строки.
 #Сохраните новое содержимое в созданном файле. 
-import sys
 
-if __name__ == "__main__":
-    litters = {
-            "Аа": 0,
-            "Бб": 0,
-            "Вв": 0,
-            "Гг": 0,
-            "Дд": 0,
-            "Ее": 0,
-            "Ёё": 0,
-            "Жж": 0,
-            "Зз": 0,
-            "Ии": 0,
-            "Йй": 0,
-            "Кк": 0,
-            "Лл": 0,
-            "Мм": 0,
-            "Нн": 0,
-            "Оо": 0,
-            "Пп": 0,
-            "Рр": 0,
-            "Сс": 0,
-            "Тт": 0,
-            "Уу": 0,
-            "Фф": 0,
-            "Хх": 0,
-            "Цц": 0,
-            "Чч": 0,
-            "Шш": 0,
-            "Щщ": 0,
-            "Ъъ": 0,
-            "Ыы": 0,
-            "Ьь": 0,
-            "Ээ": 0,
-            "Юю": 0,
-            "Яя": 0,
-        }
+def delete_comments(source_file, dest_file):
+    with open(source_file, 'r') as file:
+        lines = file.readlines() 
 
-    if len(sys.argv) != 2:
-        print(
-            "Передайте имя файла в качестве аргумента командной строки: ",
-            sys.argv,
-            len(sys.argv),
-        )
-        quit()
+    modified_lines = []
+    for line in lines:
+        modified_line = line.split("#")[0]  # Удаляем все, что идет после символа #
+        modified_lines.append(modified_line)
 
-    try:
-        with open(sys.argv[1], "r", encoding="utf-8") as f:
-            sentences = f.readlines()
+    with open(dest_file, 'w') as file:
+        file.writelines(modified_lines)
 
-            for sentence in sentences:
-                for i in sentence:
-                    i = (i.upper() + i.lower())
-                    if i in litters:
-                        litters[i] += 1
+    print("Комментарии удалены. Результат сохранен в файле", dest_file)
 
+if __name__ == '__main__':
+    source_file = input("Введите имя исходного файла: ")
+    dest_file = input("Введите имя файла назначения: ")
 
-        for key in litters.keys():
-            if (litters[key] != 0):
-                print(key, ":", litters[key])
-    
-       
-    except IOError:
-        # Отображаем ошибку, если с чтением из файла возникли проблемы
-        print("Ошибка при доступе к файлу.")
+    delete_comments(source_file, dest_file)
